cfae244cc171bb58529e9c5e824a640a763f769d
Jacqmin Sébastien <seb@tellthem.be>
2025-09-15 14:05:38 +0200

    fix: update defaultMessage for intlLabel to "Portfolio"

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -10,7 +10,7 @@ export default {
       icon: PluginIcon,
       intlLabel: {
         id: `${PLUGIN_ID}.plugin.name`,
-        defaultMessage: PLUGIN_ID,
+        defaultMessage: "Portfolio",
       },
       Component: async () => {
         const { App } = await import('./pages/App');
